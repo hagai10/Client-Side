567fb865beeb4687e50210b59e35258c161a6cb3
chagaichaimi <chagaichaimi.com>
2024-03-14 13:42:28 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import LoginPage from "./LoginPage";
+import {BrowserRouter, NavLink, Route, Routes} from "react-router-dom";
+import React from "react";
+import Dashboard from "./Dashboard";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <BrowserRouter>
+            <Routes>
+                <Route path={"/"} element={<LoginPage/>}></Route>
+                <Route path={"/dashboard"} element={<Dashboard/>}></Route>
+            </Routes>
+        </BrowserRouter>
     </div>
   );
 }
