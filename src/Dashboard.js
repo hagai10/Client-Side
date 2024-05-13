55835cf245126a3118028a252c056fd224ecd2f5
userMd <moti208227@gmail.com>
2024-05-13 22:37:53 +0300

    add legue table to Dashboard.js

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,9 +1,36 @@
 import React from "react";
+import axios from "axios";
 
-class Dashboard extends React.Component{
-    state={}
+class Dashboard extends React.Component {
+    state = {
+        teams: []
+    };
+
+    table = () => {
+        axios.get("http://localhost:8080/get-teams")
+            .then(response => {
+                if (response.data) {
+                    this.setState({ teams: response.data });
+                }
+            })
+            .catch(error => {
+                console.error('Error fetching teams:', error);
+            });
+    };
+
+
+
     render() {
-        return(<div></div>)
+        return (
+            <div>
+                <button onClick={this.table}>Upload Teams</button>
+                <h2>league table</h2>
+                {this.state.teams.map((team, index) => (
+                    <div key={index}>{team.name} | {team.score}</div>
+                ))}
+            </div>
+        );
     }
+}
 
-}export default Dashboard;
+export default Dashboard;
