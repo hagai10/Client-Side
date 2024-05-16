c96b2f1a4837375fd5de664560873e01b54c6b61
Yosi7708 <yosi7708@gmail.com>
2024-05-16 22:01:30 +0300

    -fix problem with matchTable

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import {BrowserRouter, NavLink, Route, Routes} from "react-router-dom";
 import React from "react";
 import Dashboard from "./layouts/DashboardPage/Dashboard";
 import AuthenticationTabs from "./layouts/LoginPage/AuthenticationTabs";
-import {Navbar} from "./layouts/Navbar";
 
 
 function App() {
